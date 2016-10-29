a70eccea6e3a4a8908bac25d459f1f35cb402e32
Christina Moulton <christina@teakmobile.com>
2016-10-29 14:51:17 -0400

    Fetch and display public gists

--- grokSwiftREST/GitHubAPIManager.swift
+++ grokSwiftREST/GitHubAPIManager.swift
@@ -9,6 +9,14 @@
 import Foundation
 import Alamofire
 
+enum GitHubAPIManagerError: Error {
+  case network(error: Error)
+  case apiProvidedError(reason: String)
+  case authCouldNot(reason: String)
+  case authLost(reason: String)
+  case objectSerialization(reason: String)
+}
+
 class GitHubAPIManager {
   static let sharedInstance = GitHubAPIManager()
   
@@ -19,5 +27,42 @@ class GitHubAPIManager {
           print(receivedString)
         }
     }
+  }
+  
+  func fetchPublicGists(completionHandler: @escaping (Result<[Gist]>) -> Void) {
+    Alamofire.request(GistRouter.getPublic())
+      .responseJSON { response in
+        let result = self.gistArrayFromResponse(response: response)
+        completionHandler(result)
+    }
+  }
+  
+  private func gistArrayFromResponse(response: DataResponse<Any>) -> Result<[Gist]> {
+    guard response.result.error == nil else {
+      print(response.result.error!)
+      return .failure(GitHubAPIManagerError.network(error: response.result.error!))
+    }
+    
+    // make sure we got JSON and it's an array
+    guard let jsonArray = response.result.value as? [[String: Any]] else {
+      print("didn't get array of gists object as JSON from API")
+      return .failure(GitHubAPIManagerError.objectSerialization(reason:
+        "Did not get JSON dictionary in response"))
+    }
+    
+    // check for "message" errors in the JSON because this API does that
+    if let jsonDictionary = response.result.value as? [String: Any],
+      let errorMessage = jsonDictionary["message"] as? String {
+      return .failure(GitHubAPIManagerError.apiProvidedError(reason: errorMessage))
+    }
+    
+    // turn JSON in to gists
+    var gists = [Gist]()
+    for item in jsonArray {
+      if let gist = Gist(json: item) {
+        gists.append(gist)
+      }
+    }
+    return .success(gists)
   }
 }
--- grokSwiftREST/MasterViewController.swift
+++ grokSwiftREST/MasterViewController.swift
@@ -38,7 +38,21 @@ class MasterViewController: UITableViewController {
   }
   
   func loadGists() {
-    GitHubAPIManager.sharedInstance.printPublicGists()
+    GitHubAPIManager.sharedInstance.fetchPublicGists() {
+      result in
+      guard result.error == nil else {
+        self.handleLoadGistsError(result.error!)
+        return
+      }
+      if let fetchedGists = result.value {
+        self.gists = fetchedGists
+      }
+      self.tableView.reloadData()
+    }
+  }
+  
+  func handleLoadGistsError(_ error: Error) {
+    // TODO: show error
   }
 
   override func didReceiveMemoryWarning() {
