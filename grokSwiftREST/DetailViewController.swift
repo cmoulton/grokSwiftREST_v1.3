1b189dbc46ab5e7ecb9fdc398c76eb65f7dde358
Christina Moulton <christina@teakmobile.com>
2016-10-29 14:34:19 -0400

    Created project, added Alamofire 4.0 pod

---
+++ grokSwiftREST/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  grokSwiftREST
+//
+//  Created by Christina Moulton on 2016-10-29.
+//  Copyright © 2016 Teak Mobile Inc. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail = self.detailItem {
+        if let label = self.detailDescriptionLabel {
+            label.text = detail.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.configureView()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  var detailItem: NSDate? {
+    didSet {
+        // Update the view.
+        self.configureView()
+    }
+  }
+
+
+}
+
